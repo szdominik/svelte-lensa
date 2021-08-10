1efcc7eb7a08c6f27d355c0a0d1e131f18797485
dominikblasir <dominikblasir@gmail.com>
2021-08-10 13:10:40 +0200

    feat(buttons): add upper row for apply button

--- src/components/apply-button.svelte
+++ src/components/apply-button.svelte
@@ -2,11 +2,20 @@
   export let callback;
 </script>
 
-<div class="apply-button" on:click={callback}>
-  <button type="button"> Apply </button>
+<div class="row-for-buttons">
+  <div class="apply-button" on:click={callback}>
+    <button type="button"> Apply </button>
+  </div>
 </div>
 
 <style>
+  .row-for-buttons {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 26px 32px 50px;
+  }
+
   .apply-button button {
     height: 50px;
     width: 100%;
@@ -27,6 +36,11 @@
   @media (min-width: 992px) {
     .apply-button {
       width: 200px;
+    }
+
+    .row-for-buttons {
+      flex-direction: row;
+      margin-bottom: 70px;
     }
   }
 </style>
