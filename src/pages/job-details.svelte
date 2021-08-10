5cb36a9c1ad47011568a5fc0dfcd47581edeafd3
dominikblasir <dominikblasir@gmail.com>
2021-08-10 10:51:34 +0200

    feat(jobdetails): add basic layout of job-details page

--- src/pages/job-details.svelte
+++ src/pages/job-details.svelte
@@ -14,24 +14,57 @@
 
 <Header />
 <div class="job-details">
-  <Button
-    text="Apply"
-    callback={() => {
-      console.log(highlightedJob.url);
-    }}
-  />
+  <div class="job-details-main">
+    <Button
+      text="Apply"
+      callback={() => {
+        console.log(highlightedJob.url);
+      }}
+    />
+    <div class="similar-jobs">Similar jobs in the area</div>
+    <div class="recommended-jobs" />
+  </div>
+  <div class="job-details-sidebar" />
 </div>
 
 <style>
   .job-details {
     max-width: 1354px;
     padding: 80px 15px 0;
-    text-align: center;
+  }
+
+  .job-details-sidebar {
+    display: none;
+  }
+
+  .similar-jobs {
+    margin: 10px 0;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.08;
+    color: #4a4a4a;
   }
 
   @media (min-width: 992px) {
     .job-details {
       padding: 100px 0 0 140px;
+    }
+
+    .job-details-main {
+      display: inline-block;
+      width: 60%;
+    }
+
+    .similar-jobs {
+      font-size: 24px;
+      font-weight: 600;
+    }
+
+    .job-details-sidebar {
+      display: inline-block;
+      width: 300px;
+      margin-left: 50px;
+      vertical-align: top;
     }
   }
 </style>
