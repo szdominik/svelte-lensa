5db9b488b0f633ec412283208867765ed4cf14c4
dominikblasir <dominikblasir@gmail.com>
2021-08-10 17:02:33 +0200

    refactor(job details tabs): generalize tabs' logic

--- src/components/job-details-tabs.svelte
+++ src/components/job-details-tabs.svelte
@@ -3,11 +3,26 @@
 
   export let job;
 
-  let activeMainTab = 'job';
+  const JOB_TAB = 'job';
+  const COMPANY_TAB = 'company';
+
+  const tabs = [
+    { name: 'description', label: 'Description', parent: JOB_TAB },
+    { name: 'salary', label: 'Salary', parent: JOB_TAB },
+    { name: 'skills', label: 'Skills', parent: JOB_TAB },
+    { name: 'benefits', label: 'Benefits', parent: JOB_TAB },
+    { name: 'summary', label: 'Summary', parent: COMPANY_TAB },
+    { name: 'job-trends', label: 'Job trends', parent: COMPANY_TAB },
+    { name: 'job-openings', label: 'Job openings', parent: COMPANY_TAB },
+    { name: 'closed-jobs', label: 'Closed jobs', parent: COMPANY_TAB },
+  ];
+
+  let activeMainTab = JOB_TAB;
   let activeSubTab = 'description';
 
   const changeMainTab = (activeTab) => () => {
     activeMainTab = activeTab;
+    activeSubTab = tabs.find(({ parent }) => parent === activeTab).name;
   };
 
   const changeSubTab = (activeTab) => () => {
@@ -16,66 +31,19 @@
 </script>
 
 <div class="main-tabs">
-  <h2 class:active={activeMainTab === 'job'} on:click={changeMainTab('job')}>Job</h2>
-  <h2 class:active={activeMainTab === 'company'} on:click={changeMainTab('company')}>Company</h2>
+  <h2 class:active={activeMainTab === JOB_TAB} on:click={changeMainTab(JOB_TAB)}>Job</h2>
+  <h2 class:active={activeMainTab === COMPANY_TAB} on:click={changeMainTab(COMPANY_TAB)}>Company</h2>
 </div>
 <div class="sub-tabs">
-  <h3
-    class:active={activeSubTab === 'description'}
-    class:non-visible={activeMainTab === 'company'}
-    on:click={changeSubTab('description')}
-  >
-    Description
-  </h3>
-  <h3
-    class:active={activeSubTab === 'salary'}
-    class:non-visible={activeMainTab === 'company'}
-    on:click={changeSubTab('salary')}
-  >
-    Salary
-  </h3>
-  <h3
-    class:active={activeSubTab === 'skills'}
-    class:non-visible={activeMainTab === 'company'}
-    on:click={changeSubTab('skills')}
-  >
-    Skills
-  </h3>
-  <h3
-    class:active={activeSubTab === 'benefits'}
-    class:non-visible={activeMainTab === 'company'}
-    on:click={changeSubTab('benefits')}
-  >
-    Benefits
-  </h3>
-  <h3
-    class:active={activeSubTab === 'summary'}
-    class:non-visible={activeMainTab === 'job'}
-    on:click={changeSubTab('summary')}
-  >
-    Summary
-  </h3>
-  <h3
-    class:active={activeSubTab === 'job-trends'}
-    class:non-visible={activeMainTab === 'job'}
-    on:click={changeSubTab('job-trends')}
-  >
-    Job trends
-  </h3>
-  <h3
-    class:active={activeSubTab === 'job-openings'}
-    class:non-visible={activeMainTab === 'job'}
-    on:click={changeSubTab('job-openings')}
-  >
-    Job openings
-  </h3>
-  <h3
-    class:active={activeSubTab === 'closed-jobs'}
-    class:non-visible={activeMainTab === 'job'}
-    on:click={changeSubTab('closed-jobs')}
-  >
-    Closed jobs
-  </h3>
+  {#each tabs as tab}
+    <h3
+      class:active={activeSubTab === tab.name}
+      class:non-visible={activeMainTab !== tab.parent}
+      on:click={changeSubTab(tab.name)}
+    >
+      {tab.label}
+    </h3>
+  {/each}
 </div>
 <div class="job-details-tabs-container">
   <div class="tab-content" class:non-visible={activeSubTab !== 'description'}>
