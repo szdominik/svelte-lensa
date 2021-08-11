df35cdf81416b249d6d10a4f0f80294c82103730
dominikblasir <dominikblasir@gmail.com>
2021-08-11 11:26:59 +0200

    chore(build): add babel loader to project

--- build-config/webpack.config.js
+++ build-config/webpack.config.js
@@ -36,6 +36,16 @@ module.exports = {
         },
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           /**
